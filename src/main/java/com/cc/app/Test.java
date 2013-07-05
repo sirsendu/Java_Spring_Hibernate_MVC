f57e60477ffd79d2e5e3915b57ae59ff6b5716af
Sirsendu Konar <sirsendukonar@gmail.com>
2013-07-06 00:11:11 +0530

    Add Spring Capability on a bare Java project Change Main Method to use Spring bean to print the string.

--- src/main/java/com/cc/app/Test.java
+++ src/main/java/com/cc/app/Test.java
@@ -1,13 +1,19 @@
 package com.cc.app;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.cc.app.service.NameService;
 
 public class Test {
+    public static final String[] BEAN_DEFINITIONS = {"classpath*:applicationContext.xml"};
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	   final NameService nameService = new NameService();
-       System.out.println("My Name Is :"+nameService.getMyName());
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(BEAN_DEFINITIONS);
+        NameService nameService = (NameService) context.getBean("nameService");
+        System.out.println("My Name Is :" + nameService.getMyName());
+        context.close();
+    }
 
 }
