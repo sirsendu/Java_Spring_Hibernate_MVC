61f2666d369b7fb14a66cd1ddeb637856428a3f3
Sirsendu Konar <skonar@lexmark.com>
2013-07-05 16:51:40 +0530

    A Simple Java Project. Initial Import.

---
+++ src/main/java/com/cc/app/service/NameService.java
@@ -0,0 +1,9 @@
+package com.cc.app.service;
+
+public class NameService {
+
+	
+	public static String getMyName() {
+		return "Sirsendu Konar";
+	}
+}
---
+++ src/main/java/tutorial/Test.java
@@ -0,0 +1,12 @@
+package tutorial;
+
+import com.cc.app.service.NameService;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("My Name Is :"+NameService.getMyName());
+	}
+
+}
